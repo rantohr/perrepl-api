0a0cb6a7765a7be373be1779d5507b00784bb95d
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2023-12-16 08:19:03 +0300

    Check if a user is subscribed to an offer

--- apps/subscriptions/serializers.py
+++ apps/subscriptions/serializers.py
@@ -1,9 +1,15 @@
 
 from rest_framework import serializers
 
-from .models import Plan
+from .models import Plan, Subscription
 
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
+        fields = '__all__'
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
+    class Meta:
+        model = Subscription
         fields = '__all__'
